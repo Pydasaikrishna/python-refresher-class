3f5be569b41e193c0a12c6af51db9cf75ea3128c
Pydasaikrishna <saikrishna.pyda9951@gmail.com>
2022-12-06 21:04:08 +0530

    first pythonclass

---
+++ class1.py
@@ -0,0 +1,17 @@
+# batcount = int(input("no.of bats sold: "))
+# wicketcount = int(input("no.of wickets sold: "))
+# shoescount = int(input("no.of shoes sold: "))
+# totalcount = batcount*500 + wicketcount*200 + shoescount*400
+# print("user need to pay",totalcount)
+
+
+def solveMeFirst(a,b):
+
+   return a+b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+res = solveMeFirst(num1,num2)
+print(res)
+
+
