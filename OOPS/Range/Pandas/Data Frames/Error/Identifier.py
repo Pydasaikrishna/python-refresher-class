20ab596001d460be3de3a3336d47493c20016ae4
Pydasaikrishna <saikrishna.pyda9951@gmail.com>
2022-12-21 16:15:27 +0530

    Adding the Error Folder

---
+++ OOPS/Range/Pandas/Data Frames/Error/Identifier.py
@@ -0,0 +1,7 @@
+Aditya = "College in Andhra Pradesh"
+age = 20
+# print(Aditya())
+# print(age())
+
+for x in 456.64:
+    print(x)
---
+++ OOPS/Range/Pandas/Data Frames/Error/ListIndex.py
@@ -0,0 +1,3 @@
+Student_List = ["ram","krishna","sai","Deepika","Lavanya"]
+index = "1"
+print(Student_List[index])
